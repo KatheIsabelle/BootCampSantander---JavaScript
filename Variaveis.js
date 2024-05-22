285a3d8b275201fcc1b8f0931088d0b7255ac7c3
Kathe <kathe.isabelle@inf.ufpel.edu.br>
2024-05-21 22:54:28 -0300

    Variaveis, Array, Matrizes

---
+++ Variaveis.js
@@ -0,0 +1,16 @@
+//Cadastros de clientes Formato
+// nome + idade + endereço + telefone + email
+// .js linguagem n tipada = n precisa dizer tipo da var
+
+let nomeDoCliente = "Matheus"
+let idadeDoCliente = 20
+let enderecoDoCliente = "Av Ferreira Viana, 1234"
+let telefoneDoCliente = "53 99924-3569"
+let emailDoCliente = "Matheus.S.S@gmail.com"
+
+
+console.log("Nome do cliente: " + nomeDoCliente)
+console.log("Idade do cliente: " + idadeDoCliente)
+console.log("Endereço do cliente: " + enderecoDoCliente)
+console.log("Telefone do cliente: " + telefoneDoCliente)
+console.log("Email do cliente: " + emailDoCliente)
---
+++ matriz.js
@@ -0,0 +1,20 @@
+//estrutura para matriz, indicado para dados de tipos
+//variados
+
+let infoClientesMatriz = [
+
+    ["Kathe", 28, "Barão de Butui", "Brazil"],
+    ["Fulano1", 29, "Rua 1", "Brazil"],
+    ["Fulano2", 30, "Rua 2", "Brazil"],
+
+]
+console.log("Conteúdo Linha 0 | Coluna 0")
+console.log(infoClientesMatriz[0][0]) // Linha 0 Coluna 0
+
+console.log("Conteúdo Linha 1 inteira")
+console.log (infoClientesMatriz[1]) // Linha 1 inteira
+
+//estrutura para vetor: pode armazenar diferentes
+//tipos de dados, mas preferivel botar dados do msm tipo
+
+let infoClientesVetor = ["Kathe", "Fulano1", "Fulano2"]  
