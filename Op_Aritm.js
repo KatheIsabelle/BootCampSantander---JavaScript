e2e4d2348b8ebd2c84db9e4e334e86544b4fabe1
Kathe <kathe.isabelle@inf.ufpel.edu.br>
2024-05-24 13:07:58 -0300

    operadores e expressoes logicas js

---
+++ Op_Aritm.js
@@ -0,0 +1,17 @@
+let valorA = 50;
+let valorB = 20;
+
+
+let soma = valorA + valorB;
+let subtração = valorA - valorB;
+let multiplicação = valorA * valorB;
+let divisão = valorA / valorB;
+let resto = valorA % valorB; 
+
+console.log("Testando Operadores Aritméticos\n")
+console.log("Valor A = " + valorA + " Valor B = " + valorB);
+console.log("Soma:" + soma);
+console.log("Subtração:" + subtração);
+console.log("Multiplicação:" + multiplicação);
+console.log("Divisão:" + divisão);
+console.log("Resto:" + resto);
